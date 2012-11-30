b77d4b49ba4b183c043fe8c50cc61a93e8fcae0a
Daniel Wertheim <daniel@wertheim.se>
2012-11-30 21:58:00 +0100

    Initial

---
+++ Source/Foo/GeographyFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.SqlServer.Types;
+
+namespace Foo
+{
+    public static class GeographyFactory
+    {
+        public static SqlGeography CreatePolygon(IEnumerable<Coordinates> coordinates, int srid = 4326)
+        {
+            var list = coordinates.Distinct().ToList();
+            
+            var b = new SqlGeographyBuilder();
+            b.SetSrid(srid);
+            b.BeginGeography(OpenGisGeographyType.Polygon);
+            b.BeginFigure(list[0].Latitude, list[0].Longitude);
+
+            for (var i = 1; i < list.Count; i++)
+                b.AddLine(list[i].Latitude, list[i].Longitude);
+
+            b.AddLine(list[0].Latitude, list[0].Longitude);
+            b.EndFigure();
+            b.EndGeography();
+
+            if(b.ConstructedGeography.EnvelopeAngle() > 90)
+                return b.ConstructedGeography.ReorientObject();
+
+            return b.ConstructedGeography;
+        }
+    }
+}
---
+++ Source/Foo/Tests/GeographyFactoryTests.cs
@@ -0,0 +1,67 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+
+namespace Foo.Tests
+{
+    [TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    public class GeographyFactoryTests
+    {
+        [Test]
+        public virtual void When_Coordinates_has_correct_order_and_is_closed()
+        {
+            var g = GeographyFactory.CreatePolygon(CoordinatesTestFactory.CorrectOrderAndClosed());
+
+            Approvals.Verify(g);
+        }
+
+        [Test]
+        public virtual void When_Coordinates_has_correct_order_and_is_not_closed()
+        {
+            var g = GeographyFactory.CreatePolygon(CoordinatesTestFactory.CorrectOrderNotClosed());
+
+            Approvals.Verify(g);
+        }
+
+        [Test]
+        public virtual void When_Coordinates_has_wrong_order_and_closed()
+        {
+            var g = GeographyFactory.CreatePolygon(CoordinatesTestFactory.WrongOrderAndClosed());
+
+            Approvals.Verify(g);
+        }
+
+        [Test]
+        public virtual void When_Coordinates_has_wrong_order_and_not_closed()
+        {
+            var g = GeographyFactory.CreatePolygon(CoordinatesTestFactory.WrongOrderNotClosed());
+
+            Approvals.Verify(g);
+        }
+
+        [Test]
+        public virtual void When_Coordinates_has_wrong_order_starting_south_to_north_and_many_to_west_and_back_and_is_closed()
+        {
+            var g = GeographyFactory.CreatePolygon(CoordinatesTestFactory.WrongSouthToNorthAndManyWestAndBackClosed());
+
+            Approvals.Verify(g);
+        }
+
+        [Test]
+        public virtual void When_Castle()
+        {
+            var g = GeographyFactory.CreatePolygon(CoordinatesTestFactory.Castle());
+
+            Approvals.Verify(g);
+        }
+
+        [Test]
+        public virtual void When_Start_that_is_wrong_from_the_beginning()
+        {
+            var g = GeographyFactory.CreatePolygon(CoordinatesTestFactory.StartThatDoesNotRenderWithoutFix());
+
+            Approvals.Verify(g);
+        }
+    }
+}
